98a9bef495fe6c076883457dce6855d91c0334d4
fedem-p <federicopuppo671@gmail.com>
2025-04-03 22:08:19 +0200

    linting scraper

--- src/scraper.py
+++ src/scraper.py
@@ -7,10 +7,11 @@ import time
 from urllib.parse import urlencode
 import requests
 from bs4 import BeautifulSoup
-from .fetch_makes_and_models import load_makes_from_csv
 from tqdm import tqdm
 from .constants import EXCLUDED_CARS
+from .fetch_makes_and_models import load_makes_from_csv
 
+# Correct the import order
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
@@ -42,81 +43,61 @@ class Scraper:
         all_cars = []
         for page in tqdm(
             range(1, self.config.num_pages + 1), desc="Scraping pages", unit="page"
-        ):  # Loop through the specified number of pages
+        ):
             url = self.construct_url(page, sort_method)
 
             logger.debug("============= Scraping page %s =============", page)
 
-            try:
-                response = requests.get(url, timeout=10)
-                response.raise_for_status()  # This will raise an error for bad responses
-            except requests.exceptions.Timeout as e:
-                raise ScraperException(f"Request to {url} timed out: {e}") from e
-            except requests.exceptions.RequestException as e:
-                raise ScraperException(f"An error occurred: {e}") from e
-            if response.status_code == 200:
-                # Parse the response using BeautifulSoup
+            response = self._get_response(url)
+            if response:
                 soup = BeautifulSoup(response.text, "html.parser")
                 cars = self.extract_car_data(soup)
-                all_cars.extend(cars)  # Add the cars from this page to the overall list
-            else:
-                raise ScraperException(
-                    f"Failed to fetch data from page {page}: Status code {response.status_code}"
-                )
+                all_cars.extend(cars)
 
             wait_time = random.uniform(1, 3)
             logger.debug(
                 "Waiting for %.2f seconds before the next request...", wait_time
             )
-            # print to display progress bar though the docker logs
-            logger.info(".")
+            logger.info(".")  # Print to display progress bar in docker logs
             time.sleep(wait_time)
 
         return all_cars
 
+    def _get_response(self, url):
+        """Helper method to send GET request and handle exceptions."""
+        try:
+            response = requests.get(url, timeout=10)
+            response.raise_for_status()  # This will raise an error for bad responses
+            return response
+        except requests.exceptions.Timeout as e:
+            logger.error("Request to %s timed out: %s", url, e)
+        except requests.exceptions.RequestException as e:
+            logger.error("An error occurred: %s", e)
+        return None
+
     def construct_url(self, page, sort_method):
-        """Construct the search URL with query parameters based on the filters.
-
-        Args:
-            page (int): The page number to fetch.
-
-        Returns:
-            str: The constructed URL for scraping car data.
-        """
-        # Build the URL with query parameters based on filters
+        """Construct the search URL with query parameters based on the filters."""
         params = {
             "atype": "C",  # Example: Car type
-            "body": ",".join(
-                map(str, self.config.filters.get("body", []))
-            ),  # Body types
-            "custtype": self.config.filters.get("custtype", "D"),  # Customer type
-            "cy": self.config.filters.get("country", "D"),  # Country code
-            "desc": 0,  # Description flag
-            "emclass": self.config.filters.get("emclass", ""),  # Emission class
-            "ensticker": self.config.filters.get("ensticker", ""),  # Sticker type
-            "eq": ",".join(
-                map(str, self.config.filters.get("eq", []))
-            ),  # Equipment type
-            "fregfrom": self.config.filters.get(
-                "min_year", ""
-            ),  # From year of registration
-            "kmto": self.config.filters.get("kmto", ""),  # Maximum kilometers driven
-            "powerfrom": self.config.filters.get("min_power", ""),  # Minimum power
-            "powertype": "hp",  # Power type
-            "fuel": ",".join(
-                map(str, self.config.filters.get("fuel", []))
-            ),  # fuel types
-            "page": page,  # Add the page number to the parameters
-            "priceto": self.config.filters.get("max_price", ""),  # Maximum price
-            "seatsfrom": self.config.filters.get(
-                "min_seats", ""
-            ),  # Minimum number of seats
-            "sort": sort_method,  # Sorting method
+            "body": ",".join(map(str, self.config.filters.get("body", []))),
+            "custtype": self.config.filters.get("custtype", "D"),
+            "cy": self.config.filters.get("country", "D"),
+            "desc": 0,
+            "emclass": self.config.filters.get("emclass", ""),
+            "ensticker": self.config.filters.get("ensticker", ""),
+            "eq": ",".join(map(str, self.config.filters.get("eq", []))),
+            "fregfrom": self.config.filters.get("min_year", ""),
+            "kmto": self.config.filters.get("kmto", ""),
+            "powerfrom": self.config.filters.get("min_power", ""),
+            "powertype": "hp",
+            "fuel": ",".join(map(str, self.config.filters.get("fuel", []))),
+            "page": page,
+            "priceto": self.config.filters.get("max_price", ""),
+            "seatsfrom": self.config.filters.get("min_seats", ""),
+            "sort": sort_method,
             "damaged_listing": "exclude",
             "ocs_listing": "include",
-            "ustate": ",".join(
-                self.config.filters.get("ustate", ["N", "U"])
-            ),  # Vehicle condition
+            "ustate": ",".join(self.config.filters.get("ustate", ["N", "U"])),
         }
 
         brands = self.config.filters.get("brands")
@@ -137,72 +118,72 @@ class Scraper:
             return car_list
 
         for car in listings:
-            try:
-                url = car.find("a", class_="ListItem_title__ndA4s")[
-                    "href"
-                ]  # Extract the URL
-
-                full_url = f"https://www.autoscout24.com{url}"
-                car_make = car.get("data-make", "").strip()
-                car_model = car.get("data-model", "").strip()
-                car_price = (
-                    int(
-                        car.get("data-price", "")
-                        .replace("€", "")
-                        .replace(".", "")
-                        .strip()
-                    )
-                    if car.get("data-price")
-                    else None
-                )
-                car_km = (
-                    int(
-                        car.get("data-mileage", "")
-                        .replace("km", "")
-                        .replace(",", "")
-                        .strip()
-                    )
-                    if car.get("data-mileage")
-                    else None
-                )
-                car_year = (
-                    int(car.get("data-first-registration", "").split("-")[1])
-                    if car.get("data-first-registration")
-                    else None
-                )
-
-                if car_make in EXCLUDED_CARS:
-                    if car_model in EXCLUDED_CARS[car_make]:
-                        logger.debug("skipped %s | %s", car_make, car_model)
-                        continue
-
-                # Create a dictionary for the car data
-                car_data = self.scrape_car_details(full_url)
-                if car_data:
-                    car_data.update(
-                        {
-                            "url": full_url,
-                            "make": car_make,
-                            "model": car_model,
-                            "year": car_year,
-                            "price": car_price,
-                            "mileage": car_km,
-                            "date": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
-                        }
-                    )
-                    car_list.append(car_data)
-                    logger.debug(
-                        "Scraped new car: %s | %s | %s euro | %skm",
-                        car_make,
-                        car_model,
-                        car_price,
-                        car_km,
-                    )
-
-            except (AttributeError, ValueError, IndexError, ScraperException) as e:
-                logger.error(f"Error extracting data for a car: {e}")
+            car_data = self._extract_car_details(car)
+            if car_data:
+                car_list.append(car_data)
 
         return car_list
+
+    def _extract_car_details(self, car):
+        """Helper method to extract car details from a single listing."""
+        try:
+            url = car.find("a", class_="ListItem_title__ndA4s")["href"]
+            full_url = f"https://www.autoscout24.com{url}"
+            car_make = car.get("data-make", "").strip()
+            car_model = car.get("data-model", "").strip()
+            car_price = self._parse_price(car.get("data-price"))
+            car_km = self._parse_km(car.get("data-mileage"))
+            car_year = self._parse_year(car.get("data-first-registration"))
+
+            if car_make in EXCLUDED_CARS and car_model in EXCLUDED_CARS[car_make]:
+                logger.debug("Skipped %s | %s", car_make, car_model)
+                return None
+
+            # Scrape additional details and update the car data
+            car_data = self.scrape_car_details(full_url)
+            if car_data:
+                car_data.update(
+                    {
+                        "url": full_url,
+                        "make": car_make,
+                        "model": car_model,
+                        "year": car_year,
+                        "price": car_price,
+                        "mileage": car_km,
+                        "date": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+                    }
+                )
+                logger.debug(
+                    "Scraped new car: %s | %s | %s euro | %skm",
+                    car_make,
+                    car_model,
+                    car_price,
+                    car_km,
+                )
+                return car_data
+
+        except (AttributeError, ValueError, IndexError, ScraperException) as e:
+            logger.error("Error extracting data for a car: %s", e)
+
+        return None
+
+    def _parse_price(self, price_str):
+        """Helper method to parse the car price."""
+        if price_str:
+            return int(price_str.replace("€", "").replace(".", "").strip())
+        return None
+
+    def _parse_km(self, km_str):
+        """Helper method to parse mileage."""
+        if km_str:
+            return int(km_str.replace("km", "").replace(",", "").strip())
+        return None
+
+    def _parse_year(self, year_str):
+        """Helper method to parse the car year."""
+        if year_str:
+            return int(year_str.split("-")[1])
+        return None
 
     def scrape_car_details(self, url):
         """Scrape additional details from the car's individual listing page."""
@@ -211,7 +192,6 @@ class Scraper:
             response.raise_for_status()
             soup = BeautifulSoup(response.text, "html.parser")
 
-            # Initialize variables
             details_mapping = {
                 "Body type": "body_type",
                 "Type": "car_type",
@@ -234,7 +214,6 @@ class Scraper:
                 "Fuel type": "fuel_type",
             }
 
-            # Initialize additional details dictionary
             additional_details = {key: None for key in details_mapping.values()}
             additional_details.update(
                 {
@@ -247,70 +226,71 @@ class Scraper:
                 }
             )
 
-            # Function to extract details from a section
-            def extract_details(section_id):
-                section = soup.find("section", id=section_id)
-                if section:
-                    for detail in section.find_all(
-                        "dl", class_="DataGrid_defaultDlStyle__xlLi_"
-                    ):
-                        for dt, dd in zip(detail.find_all("dt"), detail.find_all("dd")):
-                            key = dt.text.strip()
-                            if key in details_mapping:
-                                if key in ["Seats", "Doors", "Previous owner"]:
-                                    additional_details[details_mapping[key]] = int(
-                                        dd.text.strip()
-                                    )
-                                elif key == "Mileage":
-                                    additional_details["vehicle_mileage"] = int(
-                                        dd.text.strip()
-                                        .replace(" km", "")
-                                        .replace(",", "")
-                                        .strip()
-                                    )
-                                else:
-                                    additional_details[details_mapping[key]] = (
-                                        dd.text.strip()
-                                    )
-
-            # Extract details from all relevant sections
-            extract_details("basic-details-section")
-            extract_details("listing-history-section")
-            extract_details("technical-details-section")
-            extract_details("environment-details-section")
-
-            # Extract equipment details
-            equipment_section = soup.find("section", id="equipment-section")
-            if equipment_section:
-                for detail in equipment_section.find_all(
-                    "dl", class_="DataGrid_defaultDlStyle__xlLi_"
-                ):
-                    for dt, dd in zip(detail.find_all("dt"), detail.find_all("dd")):
-                        if dt.text.strip() == "Comfort & Convenience":
-                            for item in dd.find_all("li"):
-                                if "Android Auto" in item.text:
-                                    additional_details["android_auto"] = True
-                                if "Apple CarPlay" in item.text:
-                                    additional_details["car_play"] = True
-                                if "Seat heating" in item.text:
-                                    additional_details["seat_heating"] = True
-                        elif dt.text.strip() == "Safety & Security":
-                            for item in dd.find_all("li"):
-                                if "Cruise Control" in item.text:
-                                    additional_details["cruise_control"] = True
-                                if "Adaptive Cruise Control" in item.text:
-                                    additional_details["adaptive_cruise_control"] = True
-
-            # Extract image URL
-            image_gallery_div = soup.find("div", class_="image-gallery-slides")
-            if image_gallery_div:
-                sources = image_gallery_div.find_all("source")
-                for source in sources:
-                    if source.get("type") == "image/jpeg":
-                        additional_details["img_url"] = source.get("srcset")
-                        break  # Stop after finding the first JPEG
+            self._extract_details(soup, details_mapping, additional_details)
 
             return additional_details
 
         except requests.exceptions.RequestException as e:
             raise ScraperException(f"Error fetching details from {url}: {e}") from e
+
+    def _extract_details(self, soup, details_mapping, additional_details):
+        """Helper method to extract details from sections."""
+
+        def extract_details(section_id):
+            section = soup.find("section", id=section_id)
+            if section:
+                for detail in section.find_all(
+                    "dl", class_="DataGrid_defaultDlStyle__xlLi_"
+                ):
+                    for dt, dd in zip(detail.find_all("dt"), detail.find_all("dd")):
+                        key = dt.text.strip()
+                        if key in details_mapping:
+                            value = dd.text.strip()
+                            if key in ["Seats", "Doors", "Previous owner"]:
+                                additional_details[details_mapping[key]] = int(value)
+                            elif key == "Mileage":
+                                additional_details["vehicle_mileage"] = int(
+                                    value.replace(" km", "").replace(",", "").strip()
+                                )
+                            else:
+                                additional_details[details_mapping[key]] = value
+
+        extract_details("basic-details-section")
+        extract_details("listing-history-section")
+        extract_details("technical-details-section")
+        extract_details("environment-details-section")
+        self._extract_equipment_details(soup, additional_details)
+
+    def _extract_equipment_details(
+        self, soup, additional_details
+    ):  # pylint: disable=too-many-branches
+        """Extract equipment details from the equipment section."""
+        equipment_section = soup.find("section", id="equipment-section")
+        if equipment_section:  # pylint: disable=too-many-nested-blocks
+            for detail in equipment_section.find_all(
+                "dl", class_="DataGrid_defaultDlStyle__xlLi_"
+            ):
+                for dt, dd in zip(detail.find_all("dt"), detail.find_all("dd")):
+                    if dt.text.strip() == "Comfort & Convenience":
+                        for item in dd.find_all("li"):
+                            if "Android Auto" in item.text:
+                                additional_details["android_auto"] = True
+                            if "Apple CarPlay" in item.text:
+                                additional_details["car_play"] = True
+                            if "Seat heating" in item.text:
+                                additional_details["seat_heating"] = True
+                    elif dt.text.strip() == "Safety & Security":
+                        for item in dd.find_all("li"):
+                            if "Cruise Control" in item.text:
+                                additional_details["cruise_control"] = True
+                            if "Adaptive Cruise Control" in item.text:
+                                additional_details["adaptive_cruise_control"] = True
+
+        # Extract image URL
+        image_gallery_div = soup.find("div", class_="image-gallery-slides")
+        if image_gallery_div:
+            sources = image_gallery_div.find_all("source")
+            for source in sources:
+                if source.get("type") == "image/jpeg":
+                    additional_details["img_url"] = source.get("srcset")
+                    break  # Stop after finding the first JPEG
